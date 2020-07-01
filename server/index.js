30f4b891209b3a89b92a6d903f3bff16ecf87d83
zhaojinxin <474969612@qq.com>
2020-07-01 17:33:19 +0800

    实现欢迎语，聊天功能，统计人数，离开提示

--- server/index.js
+++ server/index.js
@@ -2,13 +2,37 @@ const WebSocket = require('ws')
 
 const wss = new WebSocket.Server({port: 3000})
 
+let num = 0
+
 wss.on('connection', function connection(ws) {
   console.log('one client is connected');
   ws.on('message', function(msg) {
+    const msgObj = JSON.parse(msg)
+    if(msgObj.event === 'enter') {
+      ws.name = msgObj.message
+      num ++
+    }
     // ws.send('serve:' + msg)
     wss.clients.forEach((client) => {
-      if (ws !== client && client.readyState === WebSocket.OPEN) {
-        client.send('server:' + msg)
+      if (client.readyState === WebSocket.OPEN) {
+        msgObj.name = ws.name
+        msgObj.num = num
+        client.send(JSON.stringify(msgObj))
+      }
+    })
+  })
+
+  ws.on('close', function() {
+    if(ws.name) {
+      num --
+    }
+    let msgObj = {}
+    wss.clients.forEach((client) => {
+      if (client.readyState === WebSocket.OPEN) {
+        msgObj.name = ws.name
+        msgObj.num = num
+        msgObj.event = 'out'
+        client.send(JSON.stringify(msgObj))
       }
     })
   })
